bcbbcb2b30c2a767219c065490adfe518524a3d6
Muhammad Huzaifa <huzaifayounus360@gmail.com>
2024-03-06 23:35:25 +0500

    uploading day 5

---
+++ day5/solution13/ex 13.ts
@@ -0,0 +1,8 @@
+// Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”
+let list = ["Bicycle","Motorcycle","Helicopter","Train","Red Bus"];
+
+console.log(`A ${list[0]} is a useful vehicle that helps us reach a destination without polluting the environment.`)
+console.log(`${list[1]} accidents had increasingly become a problem for countries `)
+console.log(`I have always admired the power and grace of a sleek Bell 407 ${list[2]}.`)
+console.log(`Some ${list[3]} are old and make a chugging sound, which is nice.`)
+console.log(`The ${list[4]} in Pakistan are a common sight on the roads.`)
