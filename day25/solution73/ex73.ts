a3a163924e0326dc73d1cf7e1617123ceab728ac
Muhammad Huzaifa <huzaifayounus360@gmail.com>
2024-03-26 22:54:09 +0500

    uploading day 25

---
+++ day25/solution73/ex73.ts
@@ -0,0 +1,14 @@
+// Question 73: Assigning and Updating Variables: Create a function where you declare a variable using let and assign an initial value. Then, update its value within the same function and log both the initial and updated values.
+
+// Explain & TIP: In JavaScript, you can assign a value to a variable with = and later update it as needed. This is fundamental for keeping track of changing data within your programs.
+function updateage() {
+    // age in 2020 
+    let age:number = 12;
+    console.log(`Age in 2020: ${age}`);
+
+    // updtaing age variable 
+    age = 16;
+    console.log(`Age in 2024: ${age}`);
+    
+}
+updateage()
---
+++ day25/solution74/ex74.ts
@@ -0,0 +1,18 @@
+// Question 74: Swapping Variables: Demonstrate how to swap the values of two variables. Start with variables a = 5 and b = 10, then swap their values so that a becomes 10 and b becomes 5.
+// taking two variables 
+let a: number = 1
+let b: number = 12
+// before swaping 
+console.log(`Before swaping:- a: ${a}, b: ${b}`);
+function swapValues() {
+    // making a copy of a                          
+    let copyOfa = a;
+    // assigning value of "b" in  "a"
+    a = b;
+    // assigning value of  copied "a" in  "b"
+    b = copyOfa;
+}
+// swaping value 
+swapValues()
+// after swaping 
+console.log(`After swaping:- a: ${a}, b: ${b}`);
